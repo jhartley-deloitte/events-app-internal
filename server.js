3e433a47ef853dff4dc3bf6d52132552f705215d
Kristin Muterspaw <kmuterspaw@deloitte.com>
2021-02-26 20:16:27 +0000

    Add prettier and format code

--- server.js
+++ server.js
@@ -1,121 +1,128 @@
-'use strict';
+'use strict'
 
 // express is a nodejs web server
 // https://www.npmjs.com/package/express
-const express = require('express');
+const express = require('express')
 
 // converts content in the request into parameter req.body
 // https://www.npmjs.com/package/body-parser
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 
 // create the server
-const app = express();
+const app = express()
 
 // the backend server will parse json, not a form request
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-// mock events data - for a totally definitely real solution this data should be coming 
+// mock events data - for a totally definitely real solution this data should be coming
 // from a cloud data store
 const mockEvents = {
-    events: [
-        { title: 'an event', id: 1, description: 'something really cool' },
-        { title: 'another event', id: 2, description: 'something even cooler' },
-        { title: 'another another event', id: 3, description: 'the coolest event yet' },
-        { title: 'the best event', id: 4, description: 'this event will be the best ever' }
-    ]
-};
+  events: [
+    { title: 'an event', id: 1, description: 'something really cool' },
+    { title: 'another event', id: 2, description: 'something even cooler' },
+    {
+      title: 'another another event',
+      id: 3,
+      description: 'the coolest event yet'
+    },
+    {
+      title: 'the best event',
+      id: 4,
+      description: 'this event will be the best ever'
+    }
+  ]
+}
 
 // bring in firestore
-const Firestore = require("@google-cloud/firestore");
+const Firestore = require('@google-cloud/firestore')
 
 // initialize Firestore and set project id from env var
-const firestore = new Firestore(
-    {
-        projectId: process.env.GOOGLE_CLOUD_PROJECT
-    }
-);
+const firestore = new Firestore({
+  projectId: process.env.GOOGLE_CLOUD_PROJECT
+})
 
 const addLike = (id, likes) => {
-    return firestore.collection("Events").doc(id).update({ likes })
+  return firestore.collection('Events').doc(id).update({ likes })
 }
 
 function getEvents(req, res) {
-    firestore.collection("Events").get()
-        .then((snapshot) => {
-            if (!snapshot.empty) {
-                const ret = { events: []};
-                snapshot.docs.forEach(element => {
-                    const el = element.data()
-                    el.id = element.id
-                    //el.likes = 0
-                    ret.events.push(el)
-                }, this);
-                console.log(ret);
-                res.json(ret);
-            } else {
-                 res.json(mockEvents);
-            }
-        })
-        .catch((err) => {
-            console.error('Error getting events', err);
-            res.json(mockEvents);
-        });
-};
-
+  firestore
+    .collection('Events')
+    .get()
+    .then(snapshot => {
+      if (!snapshot.empty) {
+        const ret = { events: [] }
+        snapshot.docs.forEach(element => {
+          const el = element.data()
+          el.id = element.id
+          //el.likes = 0
+          ret.events.push(el)
+        }, this)
+        console.log(ret)
+        res.json(ret)
+      } else {
+        res.json(mockEvents)
+      }
+    })
+    .catch(err => {
+      console.error('Error getting events', err)
+      res.json(mockEvents)
+    })
+}
 
 // health endpoint - returns an empty array
 app.get('/', (req, res) => {
-    res.json([]);
-});
+  res.json([])
+})
 
 // version endpoint to provide easy convient method to demonstrating tests pass/fail
 app.get('/version', (req, res) => {
-    res.json({ version: '1.0.0' });
-});
-
+  res.json({ version: '1.0.0' })
+})
 
 app.put('/event/:id/like', async (req, res) => {
-    const { id, likes } = req.body
-    await addLike(id, likes)
-    res.sendStatus(200)
+  const { id, likes } = req.body
+  await addLike(id, likes)
+  res.sendStatus(200)
 })
 
 // mock events endpoint. this would be replaced by a call to a datastore
 // if you went on to develop this as a real application.
 app.get('/events', (req, res) => {
-    getEvents(req, res);
-});
-
+  getEvents(req, res)
+})
 
 // Adds an event - in a real solution, this would insert into a cloud datastore.
 // Currently this simply adds an event to the mock array in memory
-// this will produce unexpected behavior in a stateless kubernetes cluster. 
+// this will produce unexpected behavior in a stateless kubernetes cluster.
 app.post('/event', (req, res) => {
-    // create a new object from the json data and add an id
-    const ev = { 
-        title: req.body.title, 
-        description: req.body.description
-     }
-    
-     // this will create the Events collection if it does not exist
-    firestore.collection("Events").add(ev).then(ret => {
-        console.log(ret)
-        getEvents(req, res);
-    });
+  // create a new object from the json data and add an id
+  const ev = {
+    title: req.body.title,
+    description: req.body.description
+  }
 
-});
+  // this will create the Events collection if it does not exist
+  firestore
+    .collection('Events')
+    .add(ev)
+    .then(ret => {
+      console.log(ret)
+      getEvents(req, res)
+    })
+})
 
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ message: err.message });
-});
+  console.error(err.stack)
+  res.status(500).json({ message: err.message })
+})
 
-const PORT = 8082;
+const PORT = 8082
 const server = app.listen(PORT, () => {
-    const host = server.address().address;
-    const port = server.address().port;
+  const host = server.address().address
+  const port = server.address().port
 
-    console.log(`Events app listening at http://${host}:${port}`);
-});
+  console.log(`Events app listening at http://${host}:${port}`)
+})
 
-module.exports = app;
+module.exports = app
